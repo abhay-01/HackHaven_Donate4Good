6cdc9635521ffe09494875f4a2c277ba12a184c4
abhay-01 <pratapsinghabhay999@gmail.com>
2023-07-08 11:00:23 +0530

    Images

--- Donate/script.js
+++ Donate/script.js
@@ -1,14 +1,26 @@
-const testimonials = document.querySelectorAll('.testimonial-card');
-let currentIndex = 0;
-
-function showNextTestimonial() {
-  testimonials[currentIndex].classList.remove('active');
-  currentIndex = (currentIndex + 1) % testimonials.length;
-  testimonials[currentIndex].classList.add('active');
-}
-
-// Automatically show the next testimonial every 5 seconds
-setInterval(showNextTestimonial, 5000);
-
-// Show the first testimonial initially
-testimonials[currentIndex].classList.add('active');
+jQuery(document).ready(function($) {
+    "use strict";
+    //  TESTIMONIALS CAROUSEL HOOK
+    $('#customers-testimonials').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+});
+// Path: Donate\script.js
